bbdce7717bbb691a59e3d52530d70ebc2d960510
Tj Gienger <kilrain@gmail.com>
2025-04-30 20:52:29 -0700

    updates

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -13,9 +13,9 @@ vim.keymap.set('n', 'N', 'Nzzzv', { noremap = true })
 vim.keymap.set('x', '<leader>p', [["_dP]])
 
 -- next greatest remap ever : asbjornHaland
-vim.keymap.set({ 'n', 'v' }, '<leader>y', [["+y]])
-vim.keymap.set('n', '<leader>Y', [["+Y]])
-vim.keymap.set({ 'n', 'v' }, '<leader>d', [["_d]])
+vim.keymap.set({ 'n', 'v' }, '<leader>y', '"+y', { noremap = true, silent = true})
+vim.keymap.set('n', '<leader>Y', '"+Y')
+vim.keymap.set({ 'n', 'v' }, '<leader>d', '"_d')
 
 vim.keymap.set('n', '<leader>S', function()
   Snacks.scratch.select()
